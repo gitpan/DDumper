b785fc157104c1fff7d0c0b830a1290c26f6fcf9
H.Merijn Brand <h.m.brand@xs4all.nl>
2008-10-07 19:32:26 +0000

    Import of HMBRAND/DDumper-0.16 from CPAN.
    gitpan-cpan-distribution: DDumper
    gitpan-cpan-version:      0.16
    gitpan-cpan-path:         HMBRAND/DDumper-0.16.tgz
    gitpan-cpan-author:       HMBRAND
    gitpan-cpan-maturity:     released

---
+++ examples/ddumper.pl
@@ -0,0 +1,34 @@
+#!/pro/bin/perl
+
+use strict;
+use warnings;
+
+use DDumper;
+
+my %hash = (
+    foo => "bar\x{0a}baz",
+    bar => [ 1, "mars", \@ARGV ],
+    );
+
+print "DDumper (\\%hash)\n";
+print DDumper \%hash;
+
+print "\$str = DDump (%hash)\n";
+my $str = DDump \%hash;
+print $str;
+print "\%hsh = DDump (%hash)\n";
+my %hsh = DDump \%hash;
+print DDumper \%hsh;
+
+print "DDump \\%hash\n";
+DDump \%hash;
+
+print "\$str = DDump (%hash, 5)\n";
+my $str = DDump (\%hash, 1);
+print $str;
+print "\%hsh = DDump (%hash, 5)\n";
+my %hsh = DDump (\%hash, 1);
+print DDumper \%hsh;
+
+print "DDump \\%hash, 5\n";
+DDump (\%hash, 1);
---
+++ t/40_DDump-h.t
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Test::More tests => 1;
+
+use DDumper;
+
+ok (1, "Here com tests for DDump () returning hash using XS");
+
+1;
